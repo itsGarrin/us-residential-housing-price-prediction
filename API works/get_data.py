09ac0d6d51d4d11cf3131731c9f7990c2e63fff2
Jiani He <nizijennyh@foxmail.com>
2024-12-12 21:35:18 -0500

    add operation region map

--- API works/get_data.py
+++ API works/get_data.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import datetime
-import json
 import os
-import typing
-
+import numpy as np
 import pandas as pd
 import requests
-
+import typing
+import json
+import datetime
 
 class AVParameters(typing.TypedDict, total=False):
     """
@@ -46,6 +45,7 @@ class AVParameters(typing.TypedDict, total=False):
     month: str
     symbol: str
     maturity: str
+    
 
 
 def get_alphavantage(params: AVParameters, use_cache: bool = True) -> typing.Any:
@@ -89,7 +89,6 @@ def get_alphavantage(params: AVParameters, use_cache: bool = True) -> typing.Any
         json.dump(data, f)
     return data
 
-
 def prices_daily(apikey: str, ticker: str, window: int = 252) -> pd.DataFrame:
     """
     Gets historical prices for a specified ticker from the AlphaVantage API.
@@ -113,7 +112,7 @@ def prices_daily(apikey: str, ticker: str, window: int = 252) -> pd.DataFrame:
         "symbol": ticker,
     }
 
-    json_data = get_alphavantage(params=params)
+    json_data = get_alphavantage(params = params)
     full_time_series = list(json_data["Time Series (Daily)"].items())
     windowed_time_series = full_time_series[:window][::-1]
     prices_list = [
@@ -148,12 +147,11 @@ def prices_daily(apikey: str, ticker: str, window: int = 252) -> pd.DataFrame:
     prices.rename(columns={"adj_close": f"{ticker}_adj_close"}, inplace=True)
 
     if any(
-            column not in prices.columns
-            for column in ["date", "adj_open", f"{ticker}_adj_close", "adj_high", "adj_low"]
+        column not in prices.columns
+        for column in ["date", "adj_open", f"{ticker}_adj_close", "adj_high", "adj_low"]
     ):
         raise ValueError("prices dataframe missing expected columns")
     return prices
-
 
 def federal_funds_rates(apikey: str, window: int = 52 * 8) -> pd.DataFrame:
     """
@@ -168,7 +166,7 @@ def federal_funds_rates(apikey: str, window: int = 52 * 8) -> pd.DataFrame:
     """
     if window < 0:
         raise ValueError("window size must be positive")
-
+        
     params: AVParameters = {
         "apikey": apikey,
         "datatype": "json",
@@ -177,20 +175,19 @@ def federal_funds_rates(apikey: str, window: int = 52 * 8) -> pd.DataFrame:
         "maturity": "3month"
     }
 
-    json_data = get_alphavantage(params=params)
+    json_data = get_alphavantage(params = params)
     windowed_time_series = json_data["data"][:window][::-1]
-
+    
     rates = pd.DataFrame(windowed_time_series)
     rates["date"] = pd.to_datetime(rates["date"])
     rates.rename(columns={"value": "fed_funds"}, inplace=True)
     rates["fed_funds"] = pd.to_numeric(rates["fed_funds"])
     rates["fed_funds"] = rates["fed_funds"] / 100
-    rates["fed_funds"] = (1 + rates["fed_funds"]) ** (1 / 52) - 1
+    rates["fed_funds"] = (1 + rates["fed_funds"]) ** (1 /52) - 1
 
     if any(column not in rates.columns for column in ["date", "fed_funds"]):
         raise ValueError("rates dataframe missing expected columns")
     return rates
-
 
 def treasury(apikey: str, window: int = 52 * 8) -> pd.DataFrame:
     """
@@ -205,7 +202,7 @@ def treasury(apikey: str, window: int = 52 * 8) -> pd.DataFrame:
     """
     if window < 0:
         raise ValueError("window size must be positive")
-
+        
     params: AVParameters = {
         "apikey": apikey,
         "datatype": "json",
@@ -213,9 +210,9 @@ def treasury(apikey: str, window: int = 52 * 8) -> pd.DataFrame:
         "interval": "weekly",
     }
 
-    json_data = get_alphavantage(params=params)
+    json_data = get_alphavantage(params = params)
     windowed_time_series = json_data["data"][:window][::-1]
-
+    
     rates = pd.DataFrame(windowed_time_series)
     rates["date"] = pd.to_datetime(rates["date"])
     rates.rename(columns={"value": "3monhth_treasury_yield"}, inplace=True)
@@ -225,7 +222,6 @@ def treasury(apikey: str, window: int = 52 * 8) -> pd.DataFrame:
     if any(column not in rates.columns for column in ["date", "3monhth_treasury_yield"]):
         raise ValueError("rates dataframe missing expected columns")
     return rates
-
 
 def unemployment(apikey: str, window: int = 12 * 8) -> pd.DataFrame:
     """
@@ -240,16 +236,16 @@ def unemployment(apikey: str, window: int = 12 * 8) -> pd.DataFrame:
     """
     if window < 0:
         raise ValueError("window size must be positive")
-
+        
     params: AVParameters = {
         "apikey": apikey,
         "datatype": "json",
         "function": "UNEMPLOYMENT",
     }
 
-    json_data = get_alphavantage(params=params)
+    json_data = get_alphavantage(params = params)
     windowed_time_series = json_data["data"][:window][::-1]
-
+    
     rates = pd.DataFrame(windowed_time_series)
     rates["date"] = pd.to_datetime(rates["date"])
     rates.rename(columns={"value": "unemployment"}, inplace=True)
@@ -259,7 +255,6 @@ def unemployment(apikey: str, window: int = 12 * 8) -> pd.DataFrame:
     if any(column not in rates.columns for column in ["date", "unemployment"]):
         raise ValueError("rates dataframe missing expected columns")
     return rates
-
 
 def cpi(apikey: str, window: int = 12 * 8) -> pd.DataFrame:
     """
@@ -274,16 +269,16 @@ def cpi(apikey: str, window: int = 12 * 8) -> pd.DataFrame:
     """
     if window < 0:
         raise ValueError("window size must be positive")
-
+        
     params: AVParameters = {
         "apikey": apikey,
         "datatype": "json",
         "function": "CPI",
     }
 
-    json_data = get_alphavantage(params=params)
+    json_data = get_alphavantage(params = params)
     windowed_time_series = json_data["data"][:window][::-1]
-
+    
     rates = pd.DataFrame(windowed_time_series)
     rates["date"] = pd.to_datetime(rates["date"])
     rates.rename(columns={"value": "cpi"}, inplace=True)
@@ -294,16 +289,15 @@ def cpi(apikey: str, window: int = 12 * 8) -> pd.DataFrame:
         raise ValueError("rates dataframe missing expected columns")
     return rates
 
-
 if __name__ == "__main__":
-    # key
-    # alphavantage_api_key = os.getenv("av_api_key")
+    #key
+    #alphavantage_api_key = os.getenv("av_api_key")
     alphavantage_api_key = "D32EVZCEU7HZUFYQ"
-
-    # Time horizon
+    
+    #Time horizon
     years = 8
-
-    # Monthly S&P 500 (SPY)
+    
+    #Monthly S&P 500 (SPY)
     spy = prices_daily(alphavantage_api_key, "SPY", 252 * years)[["date", "SPY_adj_close"]]
     spy["SPY_adj_close_1day"] = spy["SPY_adj_close"].shift(-1)
     spy["SPY_adj_close_1week"] = spy["SPY_adj_close"].shift(-5)
@@ -313,127 +307,126 @@ if __name__ == "__main__":
     spy["SPY_return_1week"] = (spy["SPY_adj_close_1week"] - spy["SPY_adj_close"]) / spy["SPY_adj_close"]
     spy["SPY_return_1month"] = (spy["SPY_adj_close_1month"] - spy["SPY_adj_close"]) / spy["SPY_adj_close"]
     spy["SPY_return_3month"] = (spy["SPY_adj_close_3month"] - spy["SPY_adj_close"]) / spy["SPY_adj_close"]
-    spy = spy.drop(
-        columns=["SPY_adj_close_1week", "SPY_adj_close_1month", "SPY_adj_close_1day", "SPY_adj_close_3month"])
-    spy.dropna(inplace=True)
+    spy = spy.drop(columns = ["SPY_adj_close_1week", "SPY_adj_close_1month", "SPY_adj_close_1day", "SPY_adj_close_3month"])
+    spy.dropna(inplace = True)
     print(spy)
-
-    # REIT ETF (SCHH)
+    
+    #REIT ETF (SCHH)
     reit_tickers = ["EQR", "ESS", "AVB", "INVH"]
-
+    
     reit_prices = {
         ticker: prices_daily(alphavantage_api_key, ticker, 252 * years)[["date", f"{ticker}_adj_close"]]
         for ticker in reit_tickers
     }
-
-    # get 1week/1month/3month price
+    
+    #get 1week/1month/3month price
     for key, value in reit_prices.items():
         value[f"{key}_adj_close_1day"] = value[f"{key}_adj_close"].shift(-1)
-        # value[f"{key}_return_1day"] = (value[f"{key}_adj_close_1day"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
-
+        value[f"{key}_return_1day"] = (value[f"{key}_adj_close_1day"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
+        
         value[f"{key}_adj_close_1week"] = value[f"{key}_adj_close"].shift(-5)
-        # value[f"{key}_return_1week"] = (value[f"{key}_adj_close_1week"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
-
+        value[f"{key}_return_1week"] = (value[f"{key}_adj_close_1week"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
+        
         value[f"{key}_adj_close_1month"] = value[f"{key}_adj_close"].shift(-20)
-        # value[f"{key}_return_1month"] = (value[f"{key}_adj_close_1month"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
-
+        value[f"{key}_return_1month"] = (value[f"{key}_adj_close_1month"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
+        
         value[f"{key}_adj_close_3month"] = value[f"{key}_adj_close"].shift(-60)
-        # value[f"{key}_return_3month"] = (value[f"{key}_adj_close_3month"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
-        value = value.drop(columns=[f"{key}_adj_close_1day"])
-
+        value[f"{key}_return_3month"] = (value[f"{key}_adj_close_3month"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
+        value = value.drop(columns = [f"{key}_adj_close_1day"])
+        
         value[f"{key}_adj_close_1week"] = value[f"{key}_adj_close"].shift(-5)
-        # value[f"{key}_return_1week"] = (value[f"{key}_adj_close_1week"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
-        value = value.drop(columns=[f"{key}_adj_close_1week"])
-
+        value[f"{key}_return_1week"] = (value[f"{key}_adj_close_1week"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
+        value = value.drop(columns = [f"{key}_adj_close_1week"])
+        
         value[f"{key}_adj_close_1month"] = value[f"{key}_adj_close"].shift(-20)
-        # value[f"{key}_return_1month"] = (value[f"{key}_adj_close_1month"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
-        value = value.drop(columns=[f"{key}_adj_close_1month"])
-
+        value[f"{key}_return_1month"] = (value[f"{key}_adj_close_1month"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
+        value = value.drop(columns = [f"{key}_adj_close_1month"])
+        
         value[f"{key}_adj_close_3month"] = value[f"{key}_adj_close"].shift(-60)
-        # value[f"{key}_return_3month"] = (value[f"{key}_adj_close_3month"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
-        value = value.drop(columns=[f"{key}_adj_close_3month"])
+        value[f"{key}_return_3month"] = (value[f"{key}_adj_close_3month"] - value[f"{key}_adj_close"]) / value[f"{key}_adj_close"]
+        value = value.drop(columns = [f"{key}_adj_close_3month"])
     print(reit_prices)
-
-    # merge
+    
+    #merge
     df_reit = None
     for key, value in reit_prices.items():
         if df_reit is None:
             df_reit = value
         else:
-            df_reit = df_reit.merge(value, on="date", how="outer")
+            df_reit = df_reit.merge(value, on= "date", how = "outer")
     print(df_reit)
-
-    # 1-month Fed Funds Rate
+    
+    #1-month Fed Funds Rate
     fed_funds = federal_funds_rates(alphavantage_api_key, 52 * years)
     print(fed_funds)
-
-    # 3-month treasury yield
+    
+    #3-month treasury yield
     treasury = treasury(alphavantage_api_key, 52 * years)
-    treasury["date"] = treasury["date"] + pd.Timedelta(days=2)
-
+    treasury["date"] = treasury["date"] + pd.Timedelta(days = 2)
+    
     print(treasury)
-
-    # Unemployment rate
+    
+    #Unemployment rate
     unemployment = unemployment(alphavantage_api_key, 12 * years)
     unemployment.set_index("date", inplace=True)
     unemployment = unemployment.resample("W").ffill()
-    unemployment.index = unemployment.index + pd.Timedelta(days=5)
+    unemployment.index = unemployment.index + pd.Timedelta(days = 5)
     print(unemployment)
-
-    # CPI
+    
+    #CPI
     cpi = cpi(alphavantage_api_key)
     cpi.set_index("date", inplace=True)
     cpi = cpi.resample("W").ffill()
-    cpi.index = cpi.index + pd.Timedelta(days=5)
+    cpi.index = cpi.index + pd.Timedelta(days = 5)
     print(cpi)
-
-    # merge
+    
+    #merge
     df1 = pd.merge(spy, df_reit,
-                   on=["date"],
-                   how="left")
-
+                     on = ["date"],
+                     how = "left")
+    
     df2 = pd.merge(unemployment, fed_funds,
-                   on=["date"],
-                   how="left")
-
+                     on = ["date"],
+                     how = "left")
+    
     df2 = pd.merge(df2, treasury,
-                   on=["date"],
-                   how="left")
+                     on = ["date"],
+                     how = "left")
     print(df2)
-
+    
     df1["date"] = pd.to_datetime(df1["date"])
     df2["date"] = pd.to_datetime(df2["date"])
 
     df = pd.merge(df1.sort_values("date"), df2.sort_values("date"), on="date")
-
+    
     df = pd.merge(df, cpi,
-                  on=["date"],
-                  how="left")
+                     on = ["date"],
+                     how = "left")
     df.loc[df["date"].dt.year == 2024, "cpi"] = df.loc[df["date"].dt.year == 2024, "cpi"].ffill()
-    df.dropna(inplace=True)
+    df.dropna(inplace = True)
     print(df)
+    
+    #df.to_csv("/Users/jianihe/Desktop/us-residential-housing-return-prediction/data/av_data.csv")
+    
+    # Read the Redfin data into a DataFrame
+    file_path_redfin = "/Users/jianihe/Desktop/weekly_housing_market_data_most_recent.tsv"
+    df_redfin = pd.read_csv(file_path_redfin, sep="\t")
+    
+    #Extract the required features
+    df_redfin.drop(columns=["region_type_id", "region_id"], inplace=True)
 
-    df.to_csv("../data/av_data.csv")
+    #extract weekly data by county
+    df_redfin = df_redfin[df_redfin["region_type"] == "county"]
+    df_redfin = df_redfin[df_redfin["duration"] == "1 weeks"]
+    df_redfin.drop(columns=["region_type", "duration"], inplace=True)
+    df_redfin.dropna(inplace = True)
 
-# =============================================================================
-#     # Read the Redfin data into a DataFrame
-#     file_path_redfin = "/Users/jianihe/Desktop/weekly_housing_market_data_most_recent.tsv"
-#     df_redfin = pd.read_csv(file_path_redfin, sep="\t")
-#     
-#     #Extract the required features
-#     df_redfin.drop(columns=["region_type_id", "region_id"], inplace=True)
-# 
-#     #extract weekly data by county
-#     df_redfin = df_redfin[df_redfin["region_type"] == "county"]
-#     df_redfin = df_redfin[df_redfin["duration"] == "1 weeks"]
-#     df_redfin.drop(columns=["region_type", "duration"], inplace=True)
-#     df_redfin.dropna(inplace = True)
-# 
-#     #seperate state
-#     df_redfin["State"] = df_redfin["region_name"].str.split(", ").str[1]
-#     df_redfin["region_name"] = df_redfin["region_name"].str.split(", ").str[0]
-# 
-#     #define month
-#     df_redfin["period_begin"] = pd.to_datetime(df_redfin["period_begin"])
-#     
-# =============================================================================
+    #seperate state
+    df_redfin["State"] = df_redfin["region_name"].str.split(", ").str[1]
+    df_redfin["region_name"] = df_redfin["region_name"].str.split(", ").str[0]
+
+    #define month
+    df_redfin["period_begin"] = pd.to_datetime(df_redfin["period_begin"])
+    
+    
+
